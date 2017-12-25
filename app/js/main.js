0608ada841991acbedc5e07e5a94692bf89a4c1e
10Volo <volodymyr.fenyak@gmail.com>
2017-12-25 21:17:08 +0200

    changed size

--- app/js/main.js
+++ app/js/main.js
@@ -199,3 +199,12 @@ function setRating(){
         }
     });
 }
+
+// SCROLL ON ENTER
+document.getElementById("question1").addEventListener("keydown", function(e) {
+    if (!e) { var e = window.event; }
+    e.preventDefault(); // sometimes useful
+
+    // Enter is pressed
+    if (e.keyCode == 13) { submitFunction(); }
+}, false);
