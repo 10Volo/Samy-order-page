a9e57798ac43da69ba7d85c27a5fff0a4ea6d29d
10Volo <volodymyr.fenyak@gmail.com>
2018-01-30 19:38:23 +0200

    responsive for 1366px 1280px 1024px 767px 375px

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,40 @@
-var gulp = require ('gulp');
-var sass = require ('gulp-sass');
+// var gulp = require ('gulp');
+// var sass = require ('gulp-sass');
+//
+// gulp.task('sass', function () {
+//     return gulp.src('./app/scss/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./app/css'))
+// });
+//
+// gulp.task('watch', function () {
+//     gulp.watch('./app/scss/**/*.scss', ['sass']);
+// });
+//
+// gulp.task('default',['sass', 'watch']);
 
-gulp.task('sass', function () {
-    return gulp.src('./app/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'))
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+	
+	browserSync.init({
+		server: "."
+	});
+	
+	gulp.watch("app/scss/*.scss", ['sass']);
+	gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./app/scss/**/*.scss', ['sass']);
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src("app/scss/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('default',['sass', 'watch']);
+gulp.task('default', ['serve']);
